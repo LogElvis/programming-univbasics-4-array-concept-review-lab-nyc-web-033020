e1668f0b14ed7be6f1e42c9e13a35480b2fa653a
Elvis Ning <ningelvis@gmail.com>
2020-03-05 17:35:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   counter = 0
   while counter < array.length do  
-    if array[counter] == value_to_find
-    return counter
+    if array[counter] == value_to_find then
+      return counter
+    end
     counter += 1
   end
 end
